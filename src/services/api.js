602b9a25caa4e50d3317e196aa423e8d97422c75
rahulkumarv269 <rahulkumarv269@gmail.com>
2025-01-06 01:02:10 +0530

    base url changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:4000/api/v1"
-// const BASE_URL = "https://ecommerce-backend-lac-three.vercel.app/api/v1"
+// const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://ecommerce-backend-lac-three.vercel.app/api/v1"
 
 
 export const authEndpoints = {
